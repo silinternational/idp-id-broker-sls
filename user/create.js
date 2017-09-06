482090144c06d14615955ec75f738b308322635a
Matt Henderson <matt_henderson@sil.org>
2017-09-06 10:37:15 -0400

    Add endpoint for creating a user.

---
+++ user/create.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.pars(event.body);
+  
+  if (typeof data.employee_id !== 'string') {
+    console.error('Validation failed: employee_id is not a string.');
+    callback(new Error('Could not create the user.'));
+    return;
+  }
+  const params = {
+    TableName: 'user',
+    Item: {
+      id: uuid.v1(),
+      employee_id: data.employee_id,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      display_name: data.display_name,
+      username: data.username,
+      email: data.email,
+      active: data.active,
+      locked: data.locked,
+      last_changed_utc: timestamp,
+      last_synced_utc: undefined,
+    }
+  }
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Could not create the user.'));
+      return;
+    }
+    
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(results.Item)
+    }
+    callback(null, response);
+  })
+}
