53008578469b6882950b251ef956a00e92d9fad3
Matt Henderson <matt_henderson@sil.org>
2017-09-06 12:50:23 -0400

    Add endpoint for deleting a specific user.

---
+++ user/delete.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: 'idp-id-broker.user',
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+  
+  dynamoDb.delete(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Could not delete user.'));
+      return;
+    }
+    
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({})
+    };
+    callback(null, response);
+  });
+};
