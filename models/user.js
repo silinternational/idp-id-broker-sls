6bae8e8c22bb1cb6ca9e8ee7a969a3d22a0594ca
Matt Henderson <matt_henderson@sil.org>
2017-09-07 17:05:05 -0400

    Extract logic out of user/create.js handler.

--- models/user.js
+++ models/user.js
@@ -18,6 +18,54 @@ const getUser = (employeeId, callback) => {
 };
 module.exports.getUser = getUser;
 
+const createAndReturnUser = (requestBody, callback) => {
+  const timestamp = new Date().getTime();
+  if (typeof requestBody === 'undefined') {
+    response.returnError(422, 'No request body found', callback);
+    return;
+  }
+  const data = JSON.parse(requestBody);
+  
+  if (data === null) {
+    response.returnError(422, 'Request body did not contain JSON', callback);
+    return;
+  }
+  if (typeof data.employee_id !== 'string') {
+    response.returnError(422, 'Employee ID must be a string', callback);
+    return;
+  }
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      uuid: uuid.v1(),
+      employee_id: data.employee_id,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      display_name: data.display_name,
+      username: data.username,
+      email: data.email,
+      active: data.active,
+      locked: data.locked,
+      last_changed_utc: timestamp,
+      last_synced_utc: undefined,
+    }
+  }
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Could not create the user.'));
+      return;
+    }
+    
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item)
+    }
+    callback(null, response);
+  });
+};
+module.exports.createAndReturnUser = createAndReturnUser;
+
 const returnUser = (employeeId, callback) => {
   getUser(employeeId, (error, user) => {
     if (error) {
--- user/create.js
+++ user/create.js
@@ -4,60 +4,8 @@ const uuid = require('uuid');
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+const createAndReturnUser = require('../models/user.js').createAndReturnUser;
+
 module.exports.create = (event, context, callback) => {
-  const timestamp = new Date().getTime();
-  if (typeof event.body === 'undefined') {
-    console.error('No request body found.');
-    callback(new Error('Could not create the user.'));
-    return;
-  }
-  const data = JSON.parse(event.body);
-  
-  if (data === null) {
-    console.error('No JSON data found.');
-    callback(new Error('Could not create the user.'));
-    return;
-  }
-  if (typeof data.employee_id !== 'string') {
-    console.error('Validation failed: employee_id is not a string.');
-    callback(null, {
-      statusCode: 422,
-      body: JSON.stringify({
-        "name": "Unprocessable entity",
-        "message": "Employee ID must be a string",
-        "code": 0,
-        "status": 422
-      })
-    });
-    return;
-  }
-  const params = {
-    TableName: process.env.TABLE_NAME,
-    Item: {
-      uuid: uuid.v1(),
-      employee_id: data.employee_id,
-      first_name: data.first_name,
-      last_name: data.last_name,
-      display_name: data.display_name,
-      username: data.username,
-      email: data.email,
-      active: data.active,
-      locked: data.locked,
-      last_changed_utc: timestamp,
-      last_synced_utc: undefined,
-    }
-  }
-  dynamoDb.put(params, (error, result) => {
-    if (error) {
-      console.error(error);
-      callback(new Error('Could not create the user.'));
-      return;
-    }
-    
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Item)
-    }
-    callback(null, response);
-  })
-}
+  createAndReturnUser(event.body, callback);
+};
