fbb86c77b5fe2baf04d517aef7d8e328f805940a
Matt Henderson <matt_henderson@sil.org>
2017-09-07 16:13:35 -0400

    Revise export syntax for response helper.

--- helpers/response.js
+++ helpers/response.js
@@ -1,9 +1,7 @@
-module.exports = {
-  returnItem: function(item, callback) {
-    const response = {
-      statusCode: item ? 200 : 204,
-      body: JSON.stringify(item)
-    };
-    callback(null, response);
-  }
+module.exports.returnItem = (item, callback) => {
+  const response = {
+    statusCode: item ? 200 : 204,
+    body: JSON.stringify(item)
+  };
+  callback(null, response);
 };
