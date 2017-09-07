8cbf48bd279c87f18b00b6a82df19cf52ff7b126
Matt Henderson <matt_henderson@sil.org>
2017-09-07 16:19:55 -0400

    Add response.returnError helper function.

--- helpers/response.js
+++ helpers/response.js
@@ -1,3 +1,11 @@
+module.exports.returnError = (error, callback) => {
+  const response = {
+    statusCode: 500,
+    body: JSON.stringify(error)
+  };
+  callback(null, response);
+};
+
 module.exports.returnItem = (item, callback) => {
   const response = {
     statusCode: item ? 200 : 204,
