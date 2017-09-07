9b4e1ebb632e1c923cceba2102e1b3cfb7544a23
Matt Henderson <matt_henderson@sil.org>
2017-09-07 15:27:21 -0400

    Return 204 for no-such-user responses.

--- user/get.js
+++ user/get.js
@@ -20,7 +20,7 @@ module.exports.get = (event, context, callback) => {
     }
     
     const response = {
-      statusCode: result.Item ? 200 : 404,
+      statusCode: result.Item ? 200 : 204,
       body: JSON.stringify(result.Item)
     };
     callback(null, response);
